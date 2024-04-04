b7ee42399628bfa833a71ba07775282b7eb24a6b
boredboy97 <ly.brian@hotmail.ca>
2024-04-04 08:48:06 -0400

    Created project and started Vocab class

---
+++ VocabularyControlCenter/src/Vocab.java
@@ -0,0 +1,11 @@
+import java.util.LinkedList;
+
+public class Vocab {
+    //Single linked list to store words within vocab objects
+    LinkedList<String> words = new LinkedList<String>();
+
+    //Private inner class for nodes
+    private class Node {
+
+    }
+}
