7c1b4d95e031fd41d856131019011541adc9d957
valerie-nguyen <val.nguyen08@gmail.com>
2024-04-09 21:15:09 -0400

    Added loadFile and displayMenu method

--- VocabularyControlCenter/src/Driver.java
+++ VocabularyControlCenter/src/Driver.java
@@ -5,37 +5,24 @@ import java.util.Scanner;
 public class Driver {
     public static void main(String[] args) {
 
-        Scanner sc = null;
-        String line = "";
-        String topic = "";
-        SinglyLinkedList words = null;        
-        DoublyLinkedList vocabList = new DoublyLinkedList();
+        int choice = 0;
+        Scanner myScanner = new Scanner(System.in);
+        DoublyLinkedList vocab_List = new DoublyLinkedList();
 
-        try {
-            sc = new Scanner(new FileInputStream("A3_input_file.txt"));
-            while (sc.hasNextLine()) {
-                line = sc.nextLine().trim();
-                if (line.startsWith("#")) {
-                    
-                    if (topic != null && words != null) {
-                        Vocab vocab = new Vocab(topic, words);
-                        vocabList.addAtTail(vocab);
-                    }
-                    
-                    topic = line.substring(1);
-                    words = new SinglyLinkedList();
-                } else if (!line.equals("")) {
-                    words.addAtEnd(line);
-                }
+        do {
+            displayMenu();
+            choice = myScanner.nextInt();
+
+            switch (choice) {
+                case 7:
+                    System.out.println("Enter the name of the input file:");
+                    String inputFile = myScanner.next();
+                    vocab_List = loadFile(inputFile);
+                    break;
             }
+        } while (choice != 0);
 
-        } catch (FileNotFoundException e) {
-            System.out.println("File could not be opened.");
-        }
-
-        vocabList.display();
-
-
+        myScanner.close();
 
         SinglyLinkedList list = new SinglyLinkedList();
         list.addAtHead("apple");
@@ -56,31 +43,33 @@ public class Driver {
         list.addAtEnd("strawberry");
         // list.display();
 
-        /*SinglyLinkedList l1 = new SinglyLinkedList();
-        l1.addAtHead("backpack");
-        l1.addAtHead("go(with/together)");
-        l1.addAtHead("perfume");
-        l1.addAtHead("sweatshirt");
-        l1.addAtHead("bag");
-        DoublyLinkedList dList = new DoublyLinkedList();
-        Vocab v1 = new Vocab("Clothes and Accessories", l1);
-        SinglyLinkedList l2 = new SinglyLinkedList();
-        l2.addAtHead("gold");
-        l2.addAtHead("orange");
-        l2.addAtHead("silver");
-        l2.addAtHead("black");
-        l2.addAtHead("golden");
-        Vocab v2 = new Vocab("Colours", l2);
-
-        v1.getWords().display();
-        dList.addAtHead(v1);
-        dList.display();
-        dList.addAfter(v1, v2);
-        System.out.println("After adding new vocab:");
-        dList.display();
-        dList.remove(v1.getTopic());
-        System.out.println("After removing:");
-        dList.display();*/
+        /*
+         * SinglyLinkedList l1 = new SinglyLinkedList();
+         * l1.addAtHead("backpack");
+         * l1.addAtHead("go(with/together)");
+         * l1.addAtHead("perfume");
+         * l1.addAtHead("sweatshirt");
+         * l1.addAtHead("bag");
+         * DoublyLinkedList dList = new DoublyLinkedList();
+         * Vocab v1 = new Vocab("Clothes and Accessories", l1);
+         * SinglyLinkedList l2 = new SinglyLinkedList();
+         * l2.addAtHead("gold");
+         * l2.addAtHead("orange");
+         * l2.addAtHead("silver");
+         * l2.addAtHead("black");
+         * l2.addAtHead("golden");
+         * Vocab v2 = new Vocab("Colours", l2);
+         * 
+         * v1.getWords().display();
+         * dList.addAtHead(v1);
+         * dList.display();
+         * dList.addAfter(v1, v2);
+         * System.out.println("After adding new vocab:");
+         * dList.display();
+         * dList.remove(v1.getTopic());
+         * System.out.println("After removing:");
+         * dList.display();
+         */
 
         // System.out.println(list.remove("apple"));
         // System.out.println("\nAfter removing word:");
@@ -98,5 +87,62 @@ public class Driver {
         // SinglyLinkedList copy = list.clone();
         // copy.display();
 
+    }
+
+    public static void displayMenu() {
+        System.out.println("-----------------------------");
+        System.out.println("  Vocabulary Control Center");
+        System.out.println("-----------------------------");
+        System.out.println("1. browse a topic");
+        System.out.println("2. insert a new topic before another one");
+        System.out.println("3. insert a new topic after another one");
+        System.out.println("4. remove a topic");
+        System.out.println("5. modify a topic");
+        System.out.println("6. search topics for a word");
+        System.out.println("7. load from a file");
+        System.out.println("8. show all words starting with a given letter");
+        System.out.println("9. save to file");
+        System.out.println("0. exit");
+        System.out.println("-----------------------------");
+        System.out.print("Enter Your Choice: ");
+    }
+
+    public static DoublyLinkedList loadFile(String file) {
+        Scanner sc = null;
+        String line = "";
+        String topic = "";
+        SinglyLinkedList words = null;
+        DoublyLinkedList vocab_List = new DoublyLinkedList();
+
+        try {
+            sc = new Scanner(new FileInputStream(file));
+            while (sc.hasNextLine()) {
+                line = sc.nextLine().trim();
+                if (line.startsWith("#")) {
+
+                    if (topic != null && words != null) {
+                        Vocab vocab = new Vocab(topic, words);
+                        vocab_List.addAtTail(vocab);
+                    }
+
+                    topic = line.substring(1);
+                    words = new SinglyLinkedList();
+                } else if (!line.equals("")) {
+                    words.addAtEnd(line);
+                }
+            }
+
+            // add the last vocab
+            if (topic != null && words != null) {
+                Vocab vocab = new Vocab(topic, words);
+                vocab_List.addAtTail(vocab);
+            }
+
+            System.out.println("Done loading");
+            sc.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("File could not be opened.");
+        }
+        return vocab_List;
     }
 }
